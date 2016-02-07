0ab3f72199fe527bc4136b16ed12b7ec6cf57e8b
Geoffrey Challen <challen@buffalo.edu>
2016-02-07 09:06:39 -0500

    Today's fixes.

--- src/js/onload.js
+++ src/js/onload.js
@@ -1,10 +1,12 @@
 $(function() {
+	// Generate Google Analytics events for PDFs.
   $("#content a[href$='.pdf']")
     .attr("target","_blank")
     .click(function() {
       ga('send', 'pageview', $(this).attr('href'));
     });
 
+	// Turn on popovers and toggle on interior clicks.
   $('[data-toggle="popover"]').popover();
   $('body').on('click', function (e) {
     $('[data-toggle="popover"]').each(function () {
@@ -13,21 +15,24 @@ $(function() {
       }
     });
   });
-	function loadYouTube() {
+	
+	// Lazy YouTube video loading. The iFrame API has better ways but this is
+	// fine for now.
+	$("div.youtube-container").click(function() {
 		var iframe = document.createElement("iframe");
 		iframe.setAttribute("src", "//www.youtube.com/embed/" + this.dataset.id + "?autoplay=1&border=0");
 		iframe.setAttribute("allowfullscreen", "1");
 		$(this).children("img").first().replaceWith(iframe);
 		$(this).children(".play-button").remove();
-	}
-	$("div.youtube-container").click(loadYouTube);
+	});
 	
-	function displayOnClick() {
+	// Handle showonclick divs.
+	$(".showonclick").click(function() {
 		$(this).addClass("visible");
 		$(this).children(".showonclick-button").remove();
-	}
-	$(".showonclick").click(displayOnClick);
+	});
 	
+	// Handle lazy-iframe's. Great for Google Calendar.
 	$('.lazy-iframe').each(function() {
 		var waypoint = new Waypoint.Inview({
 			element: $(this)[0],
