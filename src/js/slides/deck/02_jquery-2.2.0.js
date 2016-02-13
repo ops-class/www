75c6314288b17eead54e7522dd3c87fa11f01af2
Geoffrey Challen <challen@buffalo.edu>
2016-02-13 15:49:27 -0500

    Trying to fix deck loading again.

--- src/js/slides/deck/02_jquery-2.2.0.js
+++
@@ -1,1 +0,0 @@
-../../common/jquery-2.2.0.js
--- src/js/slides/deck/99_onload.js
+++ src/js/slides/deck/99_onload.js
@@ -1,4 +1,11 @@
 $(function() {
+	$("iframe[src^='//www.youtube.com']").each(function () {
+		var attrs = $(this).attr('src').split("?")[1].split("&");
+		var root = location.protocol + '//' + location.host;
+		if (location.protocol == 'https:' && attrs.indexOf("enablejsapi=1") != -1) {
+			$(this).attr('src', $(this).attr('src') + "&origin=" + root);
+		}
+	});
   $('iframe').each(function (e) {
 
     $(this)[0].pause = function () {
