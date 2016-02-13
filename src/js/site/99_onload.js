473a216cd4c39a475e52637fe80e18902f83382e
Geoffrey Challen <challen@buffalo.edu>
2016-02-13 14:26:14 -0500

    Better YouTube cover support.

--- src/js/site/99_onload.js
+++ src/js/site/99_onload.js
@@ -1,44 +1,64 @@
 $(function() {
 	// Generate Google Analytics events for PDFs.
-  $("#content a[href$='.pdf']")
-    .attr("target","_blank")
-    .click(function() {
-      ga('send', 'pageview', $(this).attr('href'));
-    });
+	$("#content a[href$='.pdf']")
+		.attr("target","_blank")
+		.click(function() {
+			ga('send', 'pageview', $(this).attr('href'));
+		});
 
 	// Turn on popovers and toggle on interior clicks.
-  $('[data-toggle="popover"]').popover();
-  $('body').on('click', function (e) {
-    $('[data-toggle="popover"]').each(function () {
-      if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-        $(this).popover('hide');
-      }
-    });
-  });
+	$('[data-toggle="popover"]').popover();
+	$('body').on('click', function (e) {
+		$('[data-toggle="popover"]').each(function () {
+			if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+				$(this).popover('hide');
+			}
+		});
+	});
+
 	
+	// Autoplay doesn't work on mobile devices, so convert youtube-containers
+	// to lazy-iframes.
+	if ($.browser.mobile) {
+		$("div.youtube-container").each(function() {
+			$(this).replaceWith('<div class="lazy-iframe" id="__lazy_iframe_' + $(this).data('id') + '" data-src="https://www.youtube.com/embed/' + $(this).data('id') + '?border=0"></div>');
+		});
+	}
 	// Lazy YouTube video loading. The iFrame API has better ways but this is
 	// fine for now.
 	$("div.youtube-container").click(function() {
-		var iframe = document.createElement("iframe");
-		iframe.setAttribute("src", "//www.youtube.com/embed/" + this.dataset.id + "?autoplay=1&border=0");
-		iframe.setAttribute("allowfullscreen", "1");
+		var iframe = $('<iframe src="//www.youtube.com/embed/' +
+				$(this).data('id') + '?border=0&autoplay=1" allowfullscreen></iframe>');
+		$(iframe).load(function () {
+			$(iframe).click();
+		});
 		$(this).children("img").first().replaceWith(iframe);
 		$(this).children(".play-button").remove();
+		$(this).unbind('mouseenter mouseleave');
 	});
-	
+	$("div.youtube-container").hover(function() {
+		var element = $(this).children(".play-button").first();
+		$(element).addClass("hover");
+		$(element).animate({ opacity: 1.0 }, 50);
+	}, function () {
+		var element = $(this).children(".play-button").first();
+		$(element).removeClass("hover");
+		$(element).css("opacity", 0.6);
+	});
+
 	// Handle showonclick divs.
 	$(".showonclick").click(function() {
 		$(this).addClass("visible");
 		$(this).children(".showonclick-button").remove();
 	});
-	
+
 	// Handle lazy-iframe's. Great for Google Calendar.
 	$('.lazy-iframe').each(function() {
 		var waypoint = new Waypoint.Inview({
 			element: $(this)[0],
 			enter: function(direction) {
 				var elem = $("#" + this.element.id);
-				$(elem).replaceWith('<iframe src="' + $(elem).data("src") + '"></iframe>');
+				$(elem).replaceWith('<iframe src="' + $(elem).data("src") + ' allowfullscreen"></iframe>');
 				this.destroy();
 			}
 		});
