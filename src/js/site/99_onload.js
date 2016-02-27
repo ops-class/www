4dd77f4df9c8e4118a63427d87d4096f4df87800
Geoffrey Challen <challen@buffalo.edu>
2016-02-27 11:40:43 -0500

    Throbbers and assignment videos.

--- src/js/site/99_onload.js
+++ src/js/site/99_onload.js
@@ -50,14 +50,43 @@ function processPage() {
 		$(this).addClass("visible");
 		$(this).children(".showonclick-button").remove();
 	});
-
+	
+	var throbbers = {};
 	// Handle lazy-iframe's. Great for Google Calendar.
 	$('.lazy-iframe').each(function() {
 		var waypoint = new Waypoint.Inview({
 			element: $(this)[0],
 			enter: function(direction) {
 				var elem = $("#" + this.element.id);
-				$(elem).replaceWith('<iframe src="' + $(elem).data("src") + ' allowfullscreen"></iframe>');
+				var iframeID = this.element.id + '_iframe';
+				var parentDiv = $(elem).closest('div.embed-responsive');
+				var width = $(parentDiv).outerWidth();
+				var height = $(parentDiv).outerHeight();
+				if (width > 0 && height > 0) {
+					var size = width / 5;
+					if (height / 5 < size) {
+						size = height / 5;
+					}
+					var throb = Throbber({
+						color: 'black',
+						size: size,
+						fallback: '/img/ajax-loader.gif'
+					});
+					throb.appendTo($(parentDiv).get(0));
+					var canvas = $(parentDiv).find('canvas').first();
+					$(canvas).css('margin-left', 'auto');
+					$(canvas).css('margin-right', 'auto');
+					$(canvas).css('margin-top', (height - size) / 2 + 'px');
+					throbbers[iframeID] = throb;
+					throb.start();
+				}
+				$(elem).replaceWith('<iframe id="' + iframeID +
+						'" src="' + $(elem).data("src") + ' allowfullscreen"></iframe>');
+				$('#' + iframeID).on("load", function() {
+					if (throbbers[$(this).attr('id')]) {
+						throbbers[$(this).attr('id')].stop();
+					}
+				});
 				this.destroy();
 			}
 		});
